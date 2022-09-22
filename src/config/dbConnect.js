0e07dfcb4348ff134a3bccec99f9d35efdad438f
RenanFigueiredoEdglobo <renan.oliveira@edglobo.com.br>
2022-09-22 16:04:16 -0300

    teste mongoDB

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
-
-mongoose.connect("mongodb+srv://noticiasDB:123@noticiasdb.gqnu3pk.mongodb.net/projeto-crud-noticias");
+const MONGODB_URL = process.env.MONGODB_URL || "mongodb+srv://noticiasDB:123@noticiasdb.gqnu3pk.mongodb.net/projeto-crud-noticias"
+mongoose.connect(MONGODB_URL);
 
 let db = mongoose.connection
 export default db;
