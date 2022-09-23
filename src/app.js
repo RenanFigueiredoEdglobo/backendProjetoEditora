cffbcfd5375955c93010128e317b50e83eff2a41
RenanFigueiredoEdglobo <renan.oliveira@edglobo.com.br>
2022-09-23 20:06:40 -0300

    importantdo o cors

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from "express";
 import db from "./config/dbConnect.js";
 import routes from "./routes/index.js"
+import cors from 'cors';
+const app = express();
+app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Origin","*")
+    app.use(cors())
+    next()
+})
 db.on("error",console.log.bind(console,'erro de conexão'));
 db.once("open",()=>{
     console.log("conexão com o banco bem sucedida")
 });
-const app = express();
 app.use(express.json());
-routes(app);
+routes(app)
 export default app;
