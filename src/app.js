14b9d8faf3064652fba25ee41570aeab1c0a5eff
RenanFigueiredoEdglobo <renan.oliveira@edglobo.com.br>
2022-09-26 16:55:48 -0300

    update cors

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import routes from "./routes/index.js"
 import cors from 'cors';
 const app = express();
 app.use((req,res,next)=>{
+    res.header("Access-Control-Allow-Headers", "*")
     res.header("Access-Control-Allow-Origin", '*');
     res.header("Access-Control-Allow-Methods", 'GET,PUT,POST,DELETE');
     app.use(cors())
